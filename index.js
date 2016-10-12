f9285e9ad3e9598f7192dd506be25b74ea04f69f
Elizaveta <lizasvit@gmail.com>
2016-10-12 23:50:31 +0300

    Trying to transform. BTW: which string to get from request?

--- index.js
+++ index.js
@@ -22,6 +22,17 @@ function setTime(begin, res) {
     res.set('X-Time', logged_time);
 }
 
+function transform(text, engToRus) {
+    var rus = "щ ш ч ц ю я ё ж ъ ы э а б в г д е з и й к л м н о п р с т у ф х ь".split(' '),
+        eng = "shh sh ch cz yu ya yo zh `` y' e` a b v g d e z i j k l m n o p r s t u f x `".split(' ');
+    for (var i = 0; i < rus.length; i++) {
+        text = text.split(engToRus ? eng[i] : rus[i]).join(engToRus ? rus[i] : eng[i]);
+        text = text.split(engToRus ? eng[i].toUpperCase() : rus[i].toUpperCase()).join(engToRus ?
+            rus[i].toUpperCase() : eng[i].toUpperCase());
+    }
+
+    return text;
+}
 
 app.use('/', function(req, res, next) {
     var begin = new Date().getTime();
@@ -29,17 +40,17 @@ app.use('/', function(req, res, next) {
     if (getCookie(req.headers.cookie)) {
         res.status(200);
         setTime(begin, res);
-        next();
     } else {
-        res.status(403).end();
+        res.status(403);
     }
+    next();
 }, function (req, res, next) {
     var begin = new Date().getTime();
     console.log(req.method + ", " + req.originalUrl);
     res.set('X-Request-Url', req.method + " " + req.originalUrl);
 
     setTime(begin, res);
-    res.end();
+    next();
 });
 
 /*app.use(function(err, req, res, next) {
@@ -49,12 +60,22 @@ app.use('/', function(req, res, next) {
     res.end();
 });*/
 
-app.use(function (error, req, res, next) {
+/*app.use(function (error, req, res, next) {
     if (error instanceof SyntaxError) {
         console.log(error.toString());
     } else {
         next();
     }
+});*/
+
+app.use('/', function(req, res, next) {
+    var isCyrillic = function (text) {
+        return /[а-я]/i.test(text);
+    };
+    // which string to get from request ?
+    transform("lalala", isCyrillic("lalala"));
+    // console.log(req.toString());
+    res.end();
 });
 
 app.listen(PORT, function () {
