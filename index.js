657dd1cfb2da526ca83dbebc12d0818b1bf07356
Nitori Kawashiro <nitori.kawashiro.sync@gmail.com>
2016-10-12 17:52:31 +0300

    Added sendError403() function

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+const IMAGE_ERROR_403 = 'http://t01.deviantart.net/LGMEna-IVYL1FNjkW8pAc7oJc1s=/fit-in/150x150/filters:no_upscale():origin()/pre09/2ee3/th/pre/f/2011/162/f/b/403_error_tan___uncolored_by_foxhead128-d3io641.png';
+const MESSAGE_ERROR_403 = 'Forbidden';
+
+var sendNyaError = function(response, src, msg) {
+  response.send(
+    '<!DOCTYPE html>\n' +
+    '<html lang="en">\n' +
+    '<head>\n' +
+      '<meta charset="UTF-8">\n' +
+      '<title>Error :(</title>\n' +
+    '</head>\n' +
+    '<body>\n' +
+      '<img src="' + src + '"alt="">\n' +
+      '<p>' + msg + '</p>' +
+    '</body>\n' +
+    '</html>\n'
+  );
+};
+
+var sendError403 = function(response) {
+  sendNyaError(response, IMAGE_ERROR_403, MESSAGE_ERROR_403);
+};
+
 var getCookie = function(request) {
   if (! (request.hasOwnProperty('headers'))) {
     return null;
@@ -30,8 +53,10 @@ app.use(function(request, response, next) {
   let cookie = getCookie(request);
 
   // if not authorized, then error
-  if (! isAuthorized(cookie)) {
-    res.end(403, {error: 'Not authorized'});
+  if ((cookie === null) || (! isAuthorized(cookie))) {
+    response.status(403);
+    sendError403(response);
+    response.end();
     return;
   }
 
