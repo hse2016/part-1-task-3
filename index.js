b494f840a0271a8700dbd7e293be79ecd41278a1
PreFX48 <kerrovitarr@gmail.com>
2016-10-14 16:16:56 +0300

    Simple basement for middleware functions is created

--- index.js
+++ index.js
@@ -9,5 +9,21 @@ app.listen(PORT, function () {
     console.log(`App is listen on ${PORT}`);
 });
 
+function authorize(req, res, next)) {
+
+}
+
+function startTimeLogging(req, res, next) {
+    //use res.locals for storing data
+}
+
+function endTimeLogging(req, res, next) {
+
+}
+
+function handleError(error, req, res, next){
+    res.status(403).end(error.toString()); //just for authorization error. For other errors the code may be different
+});
+
 // IMPORTANT. Это строка должна возвращать инстанс сервера
 module.exports = app;
