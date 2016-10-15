a0900498a5e2abb638052d0a5ce330160b0f0b15
vladimir <xopeck@gmail.com>
2016-10-15 20:51:41 +0300

    add fs work

--- index.js
+++ index.js
@@ -1,78 +1,100 @@
-'use strict';
+;(function() {
+  "use strict";
 
-const express = require('express');
-const app = express();
+  const express = require('express');
+  const fs = require('fs');
+  const app = express();
 
-const PORT = process.env.PORT || 4000;
+  const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, function () {
-  console.log(`App is listen on ${PORT}`);
-});
+  app.listen(PORT, function () {
+    console.log(`App is listen on ${PORT}`);
+  });
 
-// log request time
-app.use(function(req, res, next) {
-  req.requestTime = new Date().getTime();
-  next();
-});
-
-// add cookies
-app.use(function (req, res, next) {
-  let cookiesString = (req.headers.cookie) ? req.headers.cookie : "",
-      cookies = {}, _;
-  let cookiesList = cookiesString.split(';');
-  for (let i=0, size=cookiesList.length; i < size; ++i) {
-    _ = cookiesList[i].split('=');
-    cookies[_[0]] = _[1];
-  }
-  req.cookies = cookies;
-  next();
-});
-
-// check authorized
-app.use(function (req, res, next) {
-  if (isAuthorized(req)) {
+  // log request time
+  app.use(function(req, res, next) {
+    req.requestTime = new Date().getTime();
     next();
-    return;
+  });
+
+  // add cookies
+  app.use(function (req, res, next) {
+    let cookiesString = (req.headers.cookie) ? req.headers.cookie : "",
+        cookies = {}, _;
+    let cookiesList = cookiesString.split(';');
+    for (let i=0, size=cookiesList.length; i < size; ++i) {
+      _ = cookiesList[i].split('=');
+      cookies[_[0]] = _[1];
+    }
+    req.cookies = cookies;
+    next();
+  });
+
+  // check authorized
+  app.use(function (req, res, next) {
+    if (isAuthorized(req)) {
+      next();
+      return;
+    }
+    res.sendStatus(403);
+  });
+
+  // log url
+  app.use(function (req, res, next) {
+    res.setHeader('X-Request-Url', req.method+' '+req.url);
+    console.log('X-Request-Url:', req.method+' '+req.url);
+    next();
+  });
+
+  // log time
+  app.use(function (req, res, next) {
+    let date = new Date();
+    let time = date.getTime() - req.requestTime + 1;
+    res.setHeader('X-Time', time);
+    console.log('X-Time=' + time);
+    next();
+  });
+
+  function isAuthorized(request) {
+    return !!(request && request.cookies && request.cookies.authorize);
   }
-  res.sendStatus(403);
-});
 
-// log url
-app.use(function (req, res, next) {
-  res.setHeader('X-Request-Url', req.method+' '+req.url);
-  console.log('X-Request-Url:', req.method+' '+req.url);
-  next();
-});
+  // IMPORTANT. Это строка должна возвращать инстанс сервера
 
-// log time
-app.use(function (req, res, next) {
-  let date = new Date();
-  let time = date.getTime() - req.requestTime + 1;
-  res.setHeader('X-Time', time);
-  console.log('X-Time=' + time);
-  next();
-});
+  // views
 
-function isAuthorized(request) {
-  return !!(request && request.cookies && request.cookies.authorize);
-}
+  app.get(/\/v1\/(.*)/, function (req, res) {
+    let args = req.params[0]; // 0
+    // warn access
+    if (args.indexOf('..') !== -1) {
+      throw new Error();
+    }
+    console.log('args', args);
+    let path = './' + args;
+    fs.readdir(path, function (err, items) {
+      if (err) {
+        throw new Error();
+      }
+      items.push('.');
+      items.push('..');
+      let context = {
+        content: items,
+      };
+      res.send(context);
+    });
+  });
 
-// IMPORTANT. Это строка должна возвращать инстанс сервера
+  app.get(/\/.*/, function (req, res) {
+    throw new Error();
+    // res.send('Test');
+  });
 
-// views
-app.get(/\/v1\//, function (req, res) {
-  res.send('Test');
-});
+  // log error
+  app.use(function (error, req, res, next) {
+    res.setHeader('X-Request-Error', "Unknown request");
+    res.status(503).end();
+  });
 
-app.get(/\/.*/, function (req, res) {
-  throw new Error();
-  // res.send('Test');
-});
+  module.exports = app;
 
-// log error
-app.use(function (error, req, res, next) {
-  res.setHeader('X-Request-Error', "Unknown request");
-  res.status(503).end();
-});
-
-module.exports = app;
+})();
