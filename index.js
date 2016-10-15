88cbcd6368e98b5dac27968334167b8e12de5557
PreFX48 <kerrovitarr@gmail.com>
2016-10-15 21:21:23 +0300

    Execution time logging middleware created (at this moment without writing it to response header

--- index.js
+++ index.js
@@ -9,21 +9,36 @@ app.listen(PORT, function () {
     console.log(`App is listen on ${PORT}`);
 });
 
-function authorize(req, res, next)) {
-
+function authorize(req, res, next) {
+    console.log(5);
+    res = req;
+    next();
 }
 
 function startTimeLogging(req, res, next) {
-    //use res.locals for storing data
+    res = req;
+    if (!res.locals) {
+        res.locals = {};
+    }
+    res.locals.requestStartTime = new Date().getTime();
+    next();
 }
 
 function endTimeLogging(req, res, next) {
-
+    res = req;
+    let endTime = new Date().getTime();
+    let elapsedTime = endTime - req.locals.requestStartTime;
+    next();
 }
 
 function handleError(error, req, res, next){
     res.status(403).end(error.toString()); //just for authorization error. For other errors the code may be different
-});
+};
+
+app.use(startTimeLogging);
+app.use(authorize);
+app.use(endTimeLogging);
+app.use(handleError);
 
 // IMPORTANT. Это строка должна возвращать инстанс сервера
 module.exports = app;
