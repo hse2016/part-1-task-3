5f020140f2764984cfb9e5d861a47db67ef99ee6
PreFX48 <kerrovitarr@gmail.com>
2016-10-15 23:00:43 +0300

    Time logging middleware now adds an http header

--- index.js
+++ index.js
@@ -9,17 +9,15 @@ app.listen(PORT, function () {
     console.log(`App is listen on ${PORT}`);
 });
 
-function authorize(req, res, next) {
-    res = req;
-    next();
-}
-
 function startTimeLogging(req, res, next) {
-    res = req;
     if (!res.locals) {
         res.locals = {};
     }
-    res.locals.requestStartTime = new Date().getTime();
+    res.locals.requestStartTime = process.hrtime();
+    next();
+}
+
+function authorize(req, res, next) {
     next();
 }
 
@@ -29,10 +27,11 @@ function handleError(error, req, res, next){
 }
 
 function endTimeLogging(req, res, next) {
-    res = req;
-    let endTime = new Date().getTime();
-    let elapsedTime = endTime - req.locals.requestStartTime;
+    let elapsedTime = process.hrtime(res.locals.requestStartTime);
+    elapsedTime = elapsedTime[0]*1000 + (elapsedTime[1]  - elapsedTime[1] % 1000) / 1e6;
     console.log('Request handled in ' + elapsedTime + ' ms');
+    res.append('X-Time', elapsedTime.toString());
+    res.status(200).end(); //TODO: to think if it should be here
     next();
 }
 
