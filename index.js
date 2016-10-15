b7365cffe7806ae1592961871d36ae6a9bfb69af
PreFX48 <kerrovitarr@gmail.com>
2016-10-16 01:37:46 +0300

    Added class for russian-latin transliteration

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 'use strict';
 
+class Dictionary {
+    constructor() {
+        this.russians = [];
+        for (var i = 'А'.charCodeAt(0); i != 'Я'.charCodeAt(0)+1; i++) {
+            this.russians.push(String.fromCharCode(i));
+            if (String.fromCharCode(i) === 'Е') {
+                this.russians.push('Ё');
+            }
+        }
+        this.russians = this.russians.map((element) => {return element.toLowerCase();}).concat(this.russians);
+
+        this.latins = ['A', 'B', 'V', 'G', 'D', 'E', 'Jo', 'Zh', 'Z', 'I', 'J',
+                       'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H',
+                       'C', 'Ch', 'Sh', 'Shh', '#', 'Y', "'", 'Je', 'Ju', 'Ja'];
+
+        this.toLatin = {};
+        this.toRussian = {};
+        for (var i = 0; i < this.russians.length; i++) {
+            this.toLatin[this.russians[i]] = this.latins[i % this.latins.length];
+        }
+        for (var i = 0; i < this.latins.length; i++) {
+            this.toRussian[this.latins[i]] = this.russians[i];
+        }
+    }
+}
+
+
 const express = require('express');
 const app = express();
 
@@ -18,6 +45,16 @@ function startTimeLogging(req, res, next) {
 }
 
 function authorize(req, res, next) {
+    next();
+}
+
+function logRequestMethod(req, res, next) {
+    if (!res.locals) {
+        res.locals = {};
+    }
+    let request = req.method + ' ' + req.originalUrl;
+    res.append('X-Request-Url', request);
+    console.log('New request: ' + request);
     next();
 }
 
@@ -47,6 +84,7 @@ function resolve(req, res) {
 }
 
 app.use(startTimeLogging);
+app.use(logRequestMethod);
 app.use(authorize);
 app.use(handleError);
 app.use(endTimeLogging);
