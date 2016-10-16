3a5798fd25b15896768bb68829d7d4408d1eb89f
Syrose-work <syrosework@gmail.com>
2016-10-16 22:03:42 +0300

    Completed: 1. Created class for transform stream

--- index.js
+++ index.js
@@ -4,17 +4,62 @@ const express = require('express');
 const app = express();
 const Transform = require('stream').Transform;
 
+const rus = {'А' : 'A', 'Б' : 'B', 'В' : 'V', 'Г' : 'G', 'Д' : 'D', 'Е' : 'E', 'Ё' : 'Jo',
+             'Ж' : 'Zh', 'З' : 'Z', 'И' : 'I', 'Й' : 'J', 'К' : 'K', 'Л' : 'L', 'М' : 'M',
+             'Н' : 'N', 'О' : 'O', 'П' : 'P', 'Р' : 'R', 'С' : 'S', 'Т' : 'T', 'У' : 'U',
+             'Ф' : 'F', 'Х' : 'H', 'Ц' : 'C', 'Ч' : 'Ch', 'Ш' : 'Sh', 'Щ' : 'Ssh',
+             'Ъ' : '#', 'Ы' : 'Y', 'Ь' : '\'', 'Э' : 'Je', 'Ю' : 'Ju', 'Я' : 'Ja'};
+
+(function () {
+    for(let key in rus) {
+        rus[key.toLowerCase()] = rus[key].toLowerCase();
+    }
+})();
+
+const en = {'A' : 'А', 'B' : 'Б', 'C' : 'Ц', 'D' : 'Д', 'E' : 'Е', 'F' : 'Ф',
+            'G' : 'Г', 'H' : 'Х', 'I' : 'И', 'J' : 'Й', 'K' : 'К', 'L' : 'Л',
+            'M' : 'М', 'N' : 'Н', 'O' : 'О', 'P' : 'П', 'Q' : 'Я', 'R' : 'Р',
+            'S' : 'С', 'T' : 'Т', 'U' : 'У', 'V' : 'В', 'W' : 'Щ', 'X' : 'Х',
+            'Y' : 'Ы', 'Z' : 'З'};
+
+(function () {
+    for(let key in en) {
+        en[key.toLowerCase()] = en[key].toLowerCase();
+    }
+
+})();
+
 class Translitartor extends Transform {
-    constructor(options) {
+    constructor(options, lang) {
         super(options);
+        if (lang === 'en') {
+            this.dictionary = en;
+        } else {
+            this.dictionary = rus;
+        }
     }
 
     _transform(chunk, encoding, callback) {
-        
+        let str = chunk.toString('utf-8');
+        let formatted = '';
+        for(let i = 0; i < str.length; i ++) {
+            if (str[i] in this.dictionary) {
+                formatted += this.dictionary[str[i]];
+            } else {
+                formatted += str[i];
+            }
+        }
+        this.push(formatted);
+        callback();
     }
 }
 
 
+var t = new Translitartor(null, 'en');
+
+process.stdin.pipe(t).pipe(process.stdout);
+
+/*
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, function () {
@@ -107,4 +152,4 @@ app.use(function(req, res, next) {
 });
 
 // IMPORTANT. Это строка должна возвращать инстанс сервера
-module.exports = app;
+module.exports = app; */
