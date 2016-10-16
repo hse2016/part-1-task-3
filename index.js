9065751e89b0bffde809f77175a4f1fe3a1d8479
Maxim Pestryakov <maximpestryakov@gmail.com>
2016-10-17 02:28:43 +0300

    All passed

--- index.js
+++ index.js
@@ -3,8 +3,60 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const Transform = require('stream').Transform;
+
+const cyrillic_alph = require('./alph.json');
 
 const PORT = process.env.PORT || 4000;
+
+const latin_alph = {};
+for (let key in cyrillic_alph) {
+  latin_alph[cyrillic_alph[key]] = key;
+}
+const latin_keys = Object.keys(latin_alph).sort((a, b) => b.length - a.length);
+
+class Translit extends Transform {
+  static get CYRILLIC() {
+    return 0;
+  }
+
+  static get LATIN() {
+    return 1;
+  }
+
+  constructor(options) {
+    super(options);
+  }
+
+  _transform(chunk, encoding, callback) {
+    let str = chunk.toString('utf8');
+    if (typeof this.script === 'undefined') {
+      for (let i = 0; i < str.length; ++i) {
+        if (Object.keys(cyrillic_alph).indexOf(str[i]) != -1) {
+          this.script = Translit.CYRILLIC;
+          break;
+        } else if (Object.keys(latin_alph).indexOf(str[i]) != -1) {
+          this.script = Translit.LATIN;
+          break;
+        }
+      }
+    }
+    if (this.script === Translit.CYRILLIC) {
+      for (let key in cyrillic_alph) {
+        str = str.replace(new RegExp(key, 'g'), cyrillic_alph[key]);
+        str = str.replace(new RegExp(key.toUpperCase(), 'g'), cyrillic_alph[key][0].toUpperCase() + cyrillic_alph[key].substr(1));
+      }
+    } else if (this.script === Translit.LATIN) {
+      for (let i in latin_keys) {
+        let key = latin_keys[i];
+        str = str.replace(new RegExp(key, 'g'), latin_alph[key]);
+        str = str.replace(new RegExp(key[0].toUpperCase() + key.substr(1), 'g'), latin_alph[key].toUpperCase());
+      }
+    }
+    this.push(str);
+    callback();
+  }
+}
 
 app.use((req, res, next) => {
   req.requsetTime = Date.now();
@@ -54,15 +106,51 @@ app.use((req, res, next) => {
 
 app.get(/^\/v1\b/, (req, res) => {
   let path = req.originalUrl.substr(3);
-  console.log(path);
-  if (path.split('/').find(s => s === '..')) {
+  let cnt = 0;
+  path = '.' + path.split('/').reverse().filter(dir => {
+    if (dir === '..') {
+      ++cnt;
+      return false;
+    } else if (dir === '.') {
+      return false;
+    } else if (dir.length === 0) {
+      return true;
+    } else if (cnt > 0) {
+      --cnt;
+      return false;
+    } else if (cnt === 0) {
+      return true;
+    }
+  }).reverse().join('/');
+  if (cnt > 0) {
     res.append('X-Request-Error', 'Access to upper dir');
-    res.send('');
+    res.end();
   } else {
-    fs.readdir('.' + path, (err, files) => {
-      res.json({
-        content: ['.', '..'].concat(files)
-      });
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        res.append('X-Request-Error', 'Bad path');
+        res.end();
+      } else {
+        if (stats.isFile()) {
+          let translit = new Translit();
+          fs.createReadStream(path).pipe(translit);
+          let str = '';
+          translit.on('data', (chunk) => {
+            str += chunk.toString('utf-8');
+          });
+          translit.on('end', () => {
+            res.setHeader('Transfer-Encoding', 'chunked');
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ content: str }));
+          });
+        } else if (stats.isDirectory()) {
+          fs.readdir(path, (err, files) => {
+            res.json({
+              content: ['.', '..'].concat(files)
+            });
+          });
+        }
+      }
     });
   }
 });
@@ -74,7 +162,7 @@ app.use((req, res, next) => {
 
 //Error
 app.use((err, req, res, next) => {
-  //console.log(err);
+  console.log(err);
 });
 
 app.listen(PORT, function() {
