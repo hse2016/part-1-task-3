9e77475f3918d34c059d64694853fd17d925bb86
Syrose-work <syrosework@gmail.com>
2016-10-16 16:19:48 +0300

    Completed: 1. Auth middle-ware 2. Timer middle-ware 3. Simple error handler

--- index.js
+++ index.js
@@ -9,5 +9,58 @@ app.listen(PORT, function () {
     console.log(`App is listen on ${PORT}`);
 });
 
+//Set timer middleware
+app.use(function (req, res, next) {
+    req.timer = process.hrtime();
+    next();
+});
+
+//Cookie parser middleware
+app.use(function (req, res, next) {
+    req.cookies = {};
+    let array = [];
+    if (req.headers.cookie) {
+        array = req.headers.cookie.replace('; ', ';').split(';');
+    }
+    for (let i = 0; i < array.length; i ++) {
+        let cookie = array[i].split('=');
+        req.cookies[cookie[0]] = cookie[1];
+    }
+    next();
+});
+
+//Check auth middleware
+app.use(function (req, res, next) {
+    if (!req.cookies.authorize) {
+        next({
+            status : 403,
+            message : 'User unauthorized'
+        });
+    }
+    next();
+});
+
+//Set timer middleware
+app.use(function (req, res, next) {
+    let time = process.hrtime(req.timer);
+    let formatTime = Number(time[0] + '.' + time[1]).toFixed(3);
+    res.set('X-Time', formatTime);
+    console.log(formatTime);
+    next();
+});
+
+app.get('/v1', function (req, res) {
+    res.sendStatus(200);
+});
+
+app.use(function(err, req, res, next) {
+    if (err) {
+        res.status(err.status).send(err.message);
+    } else {
+        res.status(500).send('Something broke!');
+    }
+
+});
+
 // IMPORTANT. Это строка должна возвращать инстанс сервера
 module.exports = app;
