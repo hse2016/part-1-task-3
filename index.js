f395a8b4222aa883a7cdbf0919c79deda5af6c1e
Maxim Pestryakov <maximpestryakov@gmail.com>
2016-10-16 17:00:51 +0300

    Passed: error, dir lookup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 const PORT = process.env.PORT || 4000;
 
@@ -48,7 +49,32 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   //res.append('X-Time', (Date.now() - req.requsetTime) / 1000);
   res.append('X-Time', Math.random());
-  res.send('OK');
+  next();
+});
+
+app.get(/^\/v1\b/, (req, res) => {
+  let path = req.originalUrl.substr(3);
+  console.log(path);
+  if (path.split('/').find(s => s === '..')) {
+    res.append('X-Request-Error', 'Access to upper dir');
+    res.send('');
+  } else {
+    fs.readdir('.' + path, (err, files) => {
+      res.json({
+        content: ['.', '..'].concat(files)
+      });
+    });
+  }
+});
+
+app.use((req, res, next) => {
+  res.append('X-Request-Error', 'Unknown request');
+  res.sendStatus(503);
+});
+
+//Error
+app.use((err, req, res, next) => {
+  //console.log(err);
 });
 
 app.listen(PORT, function() {
