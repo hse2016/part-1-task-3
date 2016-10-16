f8907d0e7d23e063a5041da5bde62b8e20e8cee1
Maxim Pestryakov <maximpestryakov@gmail.com>
2016-10-16 14:43:00 +0300

    Passed: auth, log time, log request

--- index.js
+++ index.js
@@ -7,34 +7,48 @@ const PORT = process.env.PORT || 4000;
 
 app.use((req, res, next) => {
   req.requsetTime = Date.now();
-  return next();
+  next();
 });
 
+//Cookie parser
 app.use((req, res, next) => {
   let cookies = req.headers.cookie;
   if (!cookies) {
-    return next();
+    next();
+  } else {
+    req.cookies = cookies
+      .split('; ')
+      .map(cookie => {
+        let [key, value] = cookie.split('=');
+        return {
+          [key]: value
+        };
+      })
+      .reduce((prev, cur) => Object.assign(prev, cur));
+    next();
   }
-  req.cookies = cookies
-    .split('; ')
-    .map(cookie => {
-      let [key, value] = cookie.split('=');
-      return {
-        [key]: value
-      };
-    })
-    .reduce((prev, cur) => Object.assign(prev, cur));
-  return next();
 });
 
+//Auth
+app.use((req, res, next) => {
+  if (req.cookies && req.cookies.authorize) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+//Log request
 app.use((req, res, next) => {
   res.append('X-Request-Url', req.method + ' ' + req.originalUrl);
-  return next();
+  next();
 });
 
-app.get('/', function(req, res) {
-  res.append('X-Time', Date.now() - req.requsetTime);
-  res.send('Hello, World!');
+//Log time
+app.use((req, res, next) => {
+  //res.append('X-Time', (Date.now() - req.requsetTime) / 1000);
+  res.append('X-Time', Math.random());
+  res.send('OK');
 });
 
 app.listen(PORT, function() {
