ccc0a675f25d5afa5651ecb59f5e5103657d1e72
DAlexIV <alexiv@bookmate.com>
2016-10-16 21:15:33 +0300

    middleware part done

--- index.js
+++ index.js
@@ -9,5 +9,58 @@ app.listen(PORT, function () {
     console.log(`App is listen on ${PORT}`);
 });
 
+// logging start time
+app.use((request, response, next) => {
+    request.timing = process.hrtime();
+    next();
+});
+
+// log request
+app.use((request, response, next) => {
+    response.setHeader("X-Request-Url", request.method + " " + request.url);
+    next();
+});
+
+// handle request url errors
+app.use((request, response, next) => {
+    if (!request.url.includes("/v1/")) {
+        response.setHeader("X-Request-Error", "Unknown request");
+        response.status(503);
+    }
+
+    next();
+});
+
+// authorization
+app.use((request, response, next) => {
+    var authorizeValue = JSON.stringify(request.headers.cookie);
+
+    if (authorizeValue == undefined || !authorizeValue.includes("authorize"))
+        response.status(403);
+
+    next();
+});
+
+
+// logging finish time
+app.use((request, response, next) => {
+    response.setHeader("X-Time", parseFloat(process.hrtime(request.timing)[1] / 1000000, 10));
+    next();
+});
+
+
+
+// handle other errors
+app.use((err, req, res, next) => {
+    res.setHeader("X-Request-Error", "Unknown request");
+    res.status(503);
+});
+
+// just display hello world
+app.get('*', (request, response) => {
+    response.send('Hello from Express!')
+});
+
+
 // IMPORTANT. Это строка должна возвращать инстанс сервера
 module.exports = app;
