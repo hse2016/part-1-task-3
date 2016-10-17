8e49ce4ad7acd536dc326fa7d19f5080507f2a96
The_mad_scientist <zlysenko.makarz@gmail.com>
2016-10-17 13:18:25 +0300

    503

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+function authorize(){}
 app.listen(PORT, function () {
     console.log(`App is listen on ${PORT}`);
 });
 
+app.use(function(error, req, res, next){
+    res.setHeader('error', error.toString());
+    res.status(503).end();
+});
 // IMPORTANT. Это строка должна возвращать инстанс сервера
 module.exports = app;
