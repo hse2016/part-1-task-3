e50c9e80565084a9d659f07c33182bb1daa8189a
The_mad_scientist <zlysenko.makarz@gmail.com>
2016-10-17 22:22:50 +0300

    engrusch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 const PORT = process.env.PORT || 4000;
 
@@ -9,6 +10,11 @@ function authorize(){}
 app.listen(PORT, function () {
     console.log(`App is listen on ${PORT}`);
 });
+var engCh = ['A','B','V','G','D','E','Jo','Zh','Z','I','J','K','L','M','N','O','P','R','S','T','U','F','H','C','Ch','Sh','Shh','##','Y','"','Je','Ju','Ja',
+             'a','b','v','g','d','e','jo','zh','z','i','j','k','l','m','n','o','p','r','s','t','u','f','h','c','ch','sh','shh','#','y',''','je','ju','ja'];
+var rusCh = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я',
+             'а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я'];
+
 
 app.use(function(error, req, res, next){
     res.setHeader('error', error.toString());
