2f1fc155c5d425e5db6f63f0f70979c9d651b827
Nitori Kawashiro <nitori.kawashiro.sync@gmail.com>
2016-10-25 12:35:02 +0300

    Added encode transformer

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+let useBase64 = false;
 
-// some constants
+process.argv.forEach(
+  (val, index, array) => {
+    if (val === '--base-64') {
+      useBase64 = true;
+    }
+  }
+);
+
+if (useBase64) {
+  console.log('Using base64 encoding');
+} else {
+  console.log('Using transliteration');
+}
 
 // middlewares
 app.use(middlewares.createTimeLoggerBegin());
@@ -14,7 +27,7 @@ app.use(middlewares.createCookieChecker());
 app.use(middlewares.createHeaderLogger());
 app.use(middlewares.createTimeLoggerEnd());
 
-app.use('/v1', middlewares.createFileSeekerMiddleware());
+app.use('/v1', middlewares.createFileSeekerMiddleware(useBase64));
 app.use('/', middlewares.createNotFoundMiddleware());
 
 app.use(middlewares.createErrorMiddleware());
