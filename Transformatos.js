a08e4007eb99f20d8b5cfc0cbe2f19bbb4e4fe17
EmilFataliev <emil.fataliev2013@yandex.ru>
2016-11-03 12:16:06 +0300

    first part done :)

---
+++ Transformatos.js
@@ -0,0 +1,9 @@
+/**
+ * Created by emilfataliev on 03.11.16.
+ */
+const Transform = require('stream').Transform;
+
+class Transformatos extends Transform {
+
+
+}
--- index.js
+++ index.js
@@ -1,13 +1,81 @@
 'use strict';
-
+var fs = require('fs');
+var path = require('path');
+const Transformattor = require('./Transformatos');
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, function () {
-    console.log(`App is listen on ${PORT}`);
+    console.log('App is listen on ${PORT}');
 });
+
+//измеряем время
+app.use(function (req, res, next) {
+    req.startAt = process.hrtime();
+    next();
+});
+
+
+app.use(function (req, res, next) {
+    // req.on('/incorrect', function(err) {
+    //     // This prints the error message and stack trace to `stderr`.
+    //     res.writeHead(503, {'X-Request-Error' :  err.toString()});
+    //     console.error('X-Request-Error', err.toString());
+    //     res.end();
+    //
+    // });dfdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddeed
+    var method = req.method; //'GET'
+    var url = req.url; // /v(number)/..
+    res.setHeader('X-Request-Url', method + ' ' + url);
+    console.log('X-Request-Url: ', method + ' ' + url);
+    next();
+});
+
+app.use(function (req, res, next) {
+
+    var current_cookie = req.headers.cookie;
+
+    if (!current_cookie) {
+
+        res.sendStatus(403);
+        //return;
+    }
+
+    if (prepare_cookies(current_cookie)) {
+        next();
+    }
+
+});
+
+function prepare_cookies(cookie) {
+
+    var result = [];
+
+    cookie = cookie.split('\n');
+    for (var i = 0; i < cookie.length; ++i) {
+        var temp = cookie[i].split('=');
+        result[temp[0]] = temp;
+    }
+
+    return !!(result && result['authorize']);
+}
+
+app.use(function (req, res, next) {
+
+    var x_time = process.hrtime(req.startAt);
+    x_time = (x_time[1] / 1000).toFixed(3);
+    res.writeHead(200, {'X-Time' :  x_time});
+    console.log('X-Time: ', x_time);
+    next();
+});
+
+
+
+app.use(function (req, res, next){
+    res.end();
+});
+
 
 // IMPORTANT. Это строка должна возвращать инстанс сервера
 module.exports = app;
